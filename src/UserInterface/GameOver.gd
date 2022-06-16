5926667794c940e90cc660e0370223f79b484906
Dakota <61559292+dakotadorn12@users.noreply.github.com>
2022-06-15 22:35:13 -0500

    UI and Scene Transitions
    Multiple Scene additions, basic layouts and buttons, fade to black transitions, placeholder art/music.

---
+++ src/UserInterface/GameOver.gd
@@ -0,0 +1,15 @@
+extends Control
+
+func _ready():
+	$Play.grab_focus()
+
+func _on_Play_pressed():
+	$Transition/Background/AnimationPlayer.play("Fade")
+
+
+func _on_Credits_pressed():
+	$Transition/Background/AnimationPlayer.play("FadeCredits")
+
+
+func _on_Exit_pressed():
+	get_tree().quit()
