792192ed7b45eabddc1dbb9d009c69c0c6335167
00Her0 <reed.hambly.29@gmail.com>
2022-06-16 15:08:22 -0700

    Fix path to Settings.tscn

--- Transition.gd
+++ Transition.gd
@@ -10,4 +10,4 @@ func _on_AnimationPlayer_animation_finished(_anim_name):
 	if _anim_name == "FadeMainMenu":
 		get_tree().change_scene("res://src/UserInterface/MainMenu.tscn")
 	if _anim_name == "FadeSettings":
-		get_tree().change_scene("res://Settings.tscn")
+		get_tree().change_scene("res://src/UserInterface/Settings.tscn")
