500ee3e28483952ed87b019df7ca7a80311da6d6
Nathaniel Morihara <nathanielmorihara@gmail.com>
2022-06-29 22:42:44 -0400

    Import Scrap Metal Art (#41)

--- src/UserInterface/CoinsCounter.gd
+++ src/UserInterface/CoinsCounter.gd
@@ -4,9 +4,6 @@ onready var coins_label = $Label
 
 func _ready():
 	coins_label.set_text(str(Global.coins_collected))
-	# Static types are necessary here to avoid warnings.
-	var anim_sprite: AnimatedSprite = $AnimatedSprite
-	anim_sprite.play()
 
 
 func _process(delta):
