83c95be80b92c92900f51fb52a93ce26e2bda5d4
Nathaniel Morihara <nathanielmorihara@gmail.com>
2022-07-03 19:56:11 -0400

    Change Weather Rates and Make Lava Non-Destructive (#63)
    Making some pretty fundamental changes - here's my thinking:
    
    I feel like right now, the player doesn't have much incentive to build a structure - it's easy to just hide right before a storm and otherwise just lay blocks down to navigate from here to there.
    
    It's not worth the player's time to build anything because it's too hard to build anything sustainable (the eruptions will tear it apart) and it's easier to just hide when it comes.
    
    So the changes I'm making here are as follows:
    * Eruptions are non-descructive (so the player doesn't give up on building)
    * Eruptions are much more frequent (5s) but not escalating - so there's a constant threat (and reason to build shelter)
    * Make earthquakes just a little more frequent (cause _some_ havoc to what the player builds) and increase the pace of the game
    
    We can tweak these numbers of revert this completely, but I thought this was worth a try

--- src/Weather/LavaPiece.gd
+++ src/Weather/LavaPiece.gd
@@ -13,18 +13,20 @@ func _on_RigidBody2D_body_entered(body):
 	if body.name == 'Player':
 		emit_signal("hit_player")
 		self.queue_free()
-	elif body.name == 'TileMap':
-		# break block
-		var body_position = body.world_to_map(position)
-		body.set_cell(body_position.x,body_position.y+1,-1)
-		body.set_cell(body_position.x-1,body_position.y+1,-1)
-		body.set_cell(body_position.x+1,body_position.y+1,-1)
-		if randi() % coin_probability_denominator == 0:
-			emit_signal("place_coin", position)
-		self.queue_free()
-	else:
-		# breaking player placed blocks
-		body.queue_free()
-		if randi() % coin_probability_denominator == 0:
-			emit_signal("place_coin", position)
-		self.queue_free()
+	# Trying Lava Pieces to be non-destructive (but more frequent)
+#	elif body.name == 'TileMap':
+#		# break block
+#		var body_position = body.world_to_map(position)
+#		body.set_cell(body_position.x,body_position.y+1,-1)
+#		body.set_cell(body_position.x-1,body_position.y+1,-1)
+#		body.set_cell(body_position.x+1,body_position.y+1,-1)
+#		if randi() % coin_probability_denominator == 0:
+#			emit_signal("place_coin", position)
+#		self.queue_free()
+#	else:
+#		# breaking player placed blocks
+#		body.queue_free()
+#		if randi() % coin_probability_denominator == 0:
+#			emit_signal("place_coin", position)
+#		self.queue_free()
+	self.queue_free()
