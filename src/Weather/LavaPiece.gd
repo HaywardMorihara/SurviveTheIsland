12b8a051f0dc627901c811071c45c541e8b31039
Nathaniel Morihara <nathanielmorihara@gmail.com>
2022-07-03 20:12:17 -0400

    Make lava pieces drop ship parts again (#65)

--- src/Weather/LavaPiece.gd
+++ src/Weather/LavaPiece.gd
@@ -4,7 +4,7 @@ extends RigidBody2D
 signal place_coin(pos)
 signal hit_player()
 
-export var coin_probability_denominator := 3
+export var coin_probability_denominator := 10
 
 onready var animation_player = get_node('../Transition/Background/AnimationPlayer')
 
@@ -20,13 +20,9 @@ func _on_RigidBody2D_body_entered(body):
 #		body.set_cell(body_position.x,body_position.y+1,-1)
 #		body.set_cell(body_position.x-1,body_position.y+1,-1)
 #		body.set_cell(body_position.x+1,body_position.y+1,-1)
-#		if randi() % coin_probability_denominator == 0:
-#			emit_signal("place_coin", position)
-#		self.queue_free()
 #	else:
 #		# breaking player placed blocks
 #		body.queue_free()
-#		if randi() % coin_probability_denominator == 0:
-#			emit_signal("place_coin", position)
-#		self.queue_free()
+	if randi() % coin_probability_denominator == 0:
+		emit_signal("place_coin", position)
 	self.queue_free()
